cdef65d3b535ecebd0068f9f79296aed3dddca01
YasanayakeR <yasanayakahkarg.22@uom.lk>
2025-04-26 13:42:21 +0530

    removed develop branch

--- Models/Laundries.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication1.Models;
-
-public class Laundries
-{
-    public int Id { get; set; } 
-    public string  Name { get; set; }
-    public string  Email { get; set; }
-    public string Password { get; set; }
-}
--- Models/Users.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication1.Models;
-
-public class Users
-{
-    public int Id { get; set; }
-    public string  Name { get; set; }
-    public string  Email { get; set; }
-    public string  Password { get; set; }
-}
