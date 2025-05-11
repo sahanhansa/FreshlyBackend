c67b9bc40f1ef18ed932443e68ca25cd53e43c04
sahanhansa <sahanhansa.rcg@gmail.com>
2025-05-11 20:17:58 +0530

    added .gitignore

--- obj/Release/net8.0/FreshlyBackendNew.AssemblyInfo.cs
+++ obj/Release/net8.0/FreshlyBackendNew.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FreshlyBackendNew")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96c8fec4eb5ba3b7b3b2b5785f94baa5035af63c")]
 [assembly: System.Reflection.AssemblyProductAttribute("FreshlyBackendNew")]
