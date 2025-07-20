7abab497866e5f143e5269dee2a810091131f5bd
sahanhansa <sahanhansa.rcg@gmail.com>
2025-07-20 13:34:07 +0530

    Add GET endpoint for order statuses

--- Models/Feedback.cs
+++ Models/Feedback.cs
@@ -22,6 +22,9 @@ namespace FreshlyBackendNew.Models
         public Guid? OrderId { get; set; }
         public Guid? LaundryId { get; set; }
 
+        // Add UserId to identify which user issued the feedback
+        public Guid? UserId { get; set; }
+
         // Navigation Properties
         [ForeignKey("OrderId")]
         public Order? Order { get; set; }
