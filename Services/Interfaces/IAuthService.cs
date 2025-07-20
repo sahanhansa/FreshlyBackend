028801b52e9c1c2b1ab0c1257cd67269a6d76850
Lasini Pallewaththa <lasinipallewaththa.official@gmail.com>
2025-07-20 17:30:20 +0530

    Revert "add auth services "

--- Services/Interfaces/IAuthService.cs
+++ Services/Interfaces/IAuthService.cs
@@ -1,14 +1,14 @@
-﻿using FreshlyBackendNew.DTOs;
+﻿using FreshlyBackendNew.Models;
+using FreshlyBackendNew.DTOs;
 
 namespace FreshlyBackendNew.Services.Interfaces
 {
     public interface IAuthService
     {
-        Task<bool> IsUsernameTakenAsync(string username);
         Task<AuthResponse> LoginCustomerAsync(LoginData loginData);
+        Task<AuthResponse> LoginDriverAsync(LoginData loginData);
         Task<AuthResponse> LoginAdminAsync(LoginData loginData);
         Task<AuthResponse> LoginLaundryAsync(LoginData loginData);
-        Task<AuthResponse> LoginDriverAsync(LoginData loginData);
-        string GenerateJwtToken(string userId, string username);
+        string GenerateJwtToken(string userId, string username, string role = "User");
     }
 }
