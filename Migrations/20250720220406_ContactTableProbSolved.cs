3c0a645c1817db5960c2644f3f863a11521dc335
Lasini Pallewaththa <lasinipallewaththa.official@gmail.com>
2025-07-21 03:34:49 +0530

    contacts table prob solved and migrated

---
+++ Migrations/20250720220406_ContactTableProbSolved.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FreshlyBackendNew.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContactTableProbSolved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Customers_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Drivers_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Laundries_UserId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_UserId",
+                table: "Contacts");
+
+            migrationBuilder.UpdateData(
+                table: "Admins",
+                keyColumn: "AdminId",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 7, 20, 22, 4, 2, 561, DateTimeKind.Utc).AddTicks(5331));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Admins",
+                keyColumn: "AdminId",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 7, 20, 20, 28, 46, 957, DateTimeKind.Utc).AddTicks(4489));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_UserId",
+                table: "Contacts",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Customers_UserId",
+                table: "Contacts",
+                column: "UserId",
+                principalTable: "Customers",
+                principalColumn: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Drivers_UserId",
+                table: "Contacts",
+                column: "UserId",
+                principalTable: "Drivers",
+                principalColumn: "DriverId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Laundries_UserId",
+                table: "Contacts",
+                column: "UserId",
+                principalTable: "Laundries",
+                principalColumn: "LaundryId");
+        }
+    }
+}
