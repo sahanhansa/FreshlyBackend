8693f2f792623dbff166d3633ad750bbc98030a1
ManukaPathirana <manuka0405@gmail.com>
2025-07-22 06:57:08 +0530

    add payment

---
+++ DTOs/PaymentRequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FreshlyBackendNew.DTOs
+{
+    public class PaymentRequestDto
+    {
+        public string OrderId { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
